857b8029b57470774b96676ebb93f28cd59d7d62
Kozlov Georgy <workKozlov@yandex.ru>
2023-04-16 15:34:33 +0400

    Update new message component

--- src/layouts/chat/new-message/new-message.component.js
+++ src/layouts/chat/new-message/new-message.component.js
@@ -1,4 +1,10 @@
 import newMessageTmpl from './new-message.component.hbs';
 import './new-message.component.scss';
+import {iconButton} from "../../../components";
 
-export const newMessageComponent = () => newMessageTmpl();
+
+
+export const newMessageComponent = () => newMessageTmpl({
+  addFileButton: iconButton('plusSquare'),
+  sendMessageButton: iconButton('send'),
+});
